cb223da52e651835e96d3c552458a406479b4495
Toktieva Albina <albinawork777@gmail.com>
2025-03-22 21:30:08 +0500

    add file for static data

---
+++ data_for_tests.py
@@ -0,0 +1,6 @@
+VALIDE_BOOK = 'Harry Potter'
+VALIDE_GENRE = 'Фантастика'
+
+EMPTY_TITLE = ''
+TITLE_MORE_41_SYMBOL = 'Необыкновенные приключения фантастического профессора в параллельной реальности'
+
